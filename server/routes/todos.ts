3af33251bc149fa483b1b768f8b48b1e411be6a8
Sonia <155363911+sonia-huynh@users.noreply.github.com>
2024-03-26 11:13:40 +1300

    add get route for todoById

--- server/routes/todos.ts
+++ server/routes/todos.ts
@@ -14,3 +14,13 @@ router.get('/', async (req, res) => {
     console.log('There was an error try again!')
   }
 })
+
+router.get('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const todos = await db.getTodoById(id)
+    res.json(todos)
+  } catch (error) {
+    console.log('There was an error try again!')
+  }
+})
