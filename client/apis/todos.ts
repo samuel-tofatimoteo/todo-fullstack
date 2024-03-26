e1a5fb6631f932a78e83741d18a12fdc90fcc79b
Michael <104609759+mollmikey@users.noreply.github.com>
2024-03-26 17:02:33 +1300

    end of day

--- client/apis/todos.ts
+++ client/apis/todos.ts
@@ -2,8 +2,11 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1'
 
-export function getTodos() {
+export async function getTodos() {
   return request.get(rootUrl + '/todos').then((res) => {
     return res.body
   })
+}
+export async function addTodos(todo: string) {
+  return request.post(rootUrl + '/todos/add').send(todo)
 }
--- client/components/AddTodo.tsx
+++ client/components/AddTodo.tsx
@@ -1,12 +1,33 @@
 // eslint-disable-next-line no-unused-vars
+import { useState } from 'react'
+import { Todos } from '../../models/model'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { addTodo } from '../../server/db/db'
+
 function AddTodo() {
+  const [todo, setTodo] = useState('')
+  const [submit, setSubmit] = useState('')
+
+  const qc = useQueryClient()
+  const mutation = useMutation({
+    mutationFn: (todo) => addTodo(todo),
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['todos'] }),
+  })
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    onSubmit(submit)
+  }
   return (
     <>
-      <input
-        className="new-todo"
-        placeholder="What needs to be done?"
-        autoFocus={true}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus={true}
+        />
+        <input type="number" name="priority" id="priority"></input>
+      </form>
     </>
   )
 }
