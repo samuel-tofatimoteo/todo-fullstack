f0b7c3ba953cc846d552fc745980d2e69fc00c55
Cae <gemwright@proton.me>
2024-03-26 18:31:27 +1300

    BAD but functional multi input form - to refactor

--- client/components/AddTodo.tsx
+++ client/components/AddTodo.tsx
@@ -6,18 +6,40 @@ import { Task } from "../../Models/Task"
 
 function AddTodo() {
 
+  // REFACTOR FORM
   const [newTask, setNewTask] = useState('')
+  const [newDetails, setnewDetails] = useState('')
+  const [newPriority, setNewPriority] = useState(0)
   const [submittedTask, setSubmittedTask] = useState('')
+  const [newForm, setNewForm] = useState({ 
+    name: '', 
+    details: '', 
+    priority: 1, 
+    completed: false }
+  )
 
-  function handleChange(e){
+  function handleChangeTask(e){
     setNewTask(e.target.value)
+  }
+
+  function handleChangeDetails(e){
+    setnewDetails(e.target.value)
+  }
+
+  function handleChangePriority(e){
+    setNewPriority(e.target.value)
   }
 
   function handleSubmit(e){
     e.preventDefault()
-    taskAdd.mutate({ name: newTask, details: 'test', priority: 1, completed: false })
-    setSubmittedTask(newTask)
+    taskAdd.mutate({ 
+      name: newTask, 
+      details: newDetails, 
+      priority: newPriority, 
+      completed: false })
     setNewTask('')
+    setnewDetails('')
+    setNewPriority(0)
   }
 
   const queryClient = useQueryClient()
@@ -32,12 +54,17 @@ function AddTodo() {
 
   return (
     <>
-      <p>New Task: {newTask}</p>
-      <p>Submitted Task: {submittedTask}</p>
+      <p>New Form: {newTask} - {newDetails} - {newPriority}</p>
+      {/* <p>Submitted Task: {submittedTask}</p> */}
       <form onSubmit={handleSubmit}>
         <label htmlFor="task">Task Name</label>
-        <input id="task" value={newTask} onChange={handleChange}
+        <input id="task" value={newTask} onChange={handleChangeTask}
         placeholder="Enter new task ..."/>
+        <label htmlFor="details">Details</label>
+        <input id="task" value={newDetails} onChange={handleChangeDetails}
+        placeholder="More about task here ..."/>
+        <label htmlFor="priority">Priority</label>
+        <input id="priority" value={newPriority} onChange={handleChangePriority}/>
       <button type="submit">Submit</button>
       </form>
       
