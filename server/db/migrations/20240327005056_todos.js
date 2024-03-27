01bda2510dd4503a857b5721017f759ff45f87de
Katie Davies <millerkatie1990@gmail.com>
2024-03-27 14:21:33 +1300

    add migration table todo

---
+++ server/db/migrations/20240327005056_todos.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('todos', (table) => {
+    table.integer('id').primary()
+    table.string('todo')
+    table.boolean('completed')
+    table.string('priority')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('todos')
+}
