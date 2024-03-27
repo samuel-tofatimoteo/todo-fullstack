1dcde9b458314c1b11689bc4a408e2d08c685380
Katie Davies <millerkatie1990@gmail.com>
2024-03-27 14:36:10 +1300

    All db queries work

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,12 @@
 import express from 'express'
 import * as Path from 'node:path'
+import * as db from './db/db'
 
 const server = express()
+
+// const todos = await db.getTodoById(3)
+
+// console.log(todos)
 
 server.use(express.json())
 
