b5dee8ba235612719bf22b22287aab17092f70d9
Katie Davies <millerkatie1990@gmail.com>
2024-03-27 14:45:17 +1300

    set up server routes

--- server/routes/todos.ts
+++ server/routes/todos.ts
@@ -1,1 +1,67 @@
+import * as db from '../db/db'
+import express from 'express'
+import { TodoId, Todos } from '../../models/todos'
 
+const router = express.Router()
+
+//GET all todos
+router.get('/', async (req, res) => {
+  try {
+    const todos = db.getAllTodos()
+    res.json(todos)
+  } catch (error) {
+    res.sendStatus(500)
+    console.log('There was an error retrieving the data')
+  }
+})
+
+//GET a todo by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const todos = db.getTodoById(id)
+    res.json(todos)
+  } catch (error) {
+    res.sendStatus(500)
+    console.log('There was an error retrieving the data')
+  }
+})
+
+//POST add a new todo
+router.post('/', async (req, res) => {
+  try {
+    const newTodo = req.body
+    const todos = db.addTodo(newTodo)
+    res.json(todos)
+  } catch (error) {
+    res.sendStatus(500)
+    console.log('There was an error retrieving the data')
+  }
+})
+
+//PATCH update a todo
+router.patch('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const updateTodo = req.body
+    const todos = db.updateTodo(id, updateTodo)
+    res.json(todos)
+  } catch (error) {
+    res.sendStatus(500)
+    console.log('There was an error retrieving the data')
+  }
+})
+
+//DELETE a todo
+router.delete('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const todos = db.deleteTodo(id)
+    res.json(todos)
+  } catch (error) {
+    res.sendStatus(500)
+    console.log('There was an error retrieving the data')
+  }
+})
+
+export default router
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import * as Path from 'node:path'
 import * as db from './db/db'
+import todos from './routes/todos'
 
 const server = express()
 
@@ -9,6 +10,7 @@ const server = express()
 // console.log(todos)
 
 server.use(express.json())
+server.use('/api/v1/todos', todos)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
