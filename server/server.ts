889432bcc8c5a2658639cf4ceac4f0007f9e9a54
Cae <gemwright@proton.me>
2024-03-27 16:41:32 +1300

    added files for dokku deploy

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,9 @@ const server = express()
 
 server.use(express.json())
 
+// route path
+server.use('/api/v1/', routes)
+
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
@@ -15,7 +18,6 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-// route path
-server.use('/api/v1/', routes)
 
 export default server
+
