786829cf01fb6a3e9f812044a520b097400df725
Sam <121822122+samuel-tofatimoteo@users.noreply.github.com>
2024-04-03 11:30:02 +1300

    Decided to make API call functions inside React hooks

--- client/apis/tasks.ts
+++ client/apis/tasks.ts
@@ -1,28 +1,52 @@
 import request from 'superagent'
 import { Task } from '../../models/Task'
+import { useQuery } from '@tanstack/react-query'
 
-export async function getTasks() {
-  const res = await request.get('api/v1/tasks')
-  return res.body as Task[]
+// export async function getTasks() {
+//   const res = await request.get('api/v1/tasks')
+//   return res.body as Task[]
+// }
+
+// export async function getTaskById(id: number) {
+//   const res = await request.get(`api/v1/tasks/${id}`)
+//   return res.body as Task
+// }
+
+// export async function addTask(newTask: Task) {
+//   return await request.post('api/v1/tasks').send(newTask)
+// }
+
+// export async function delTask(id: number) {
+//   return await request.del(`api/v1/tasks/${id}`)
+// }
+
+// export async function updateTask(taskUpdate: Task) {
+//   const { id, name, details, difficulty, completed } = taskUpdate
+//   const res = await request
+//     .patch(`api/v1/tasks/${id}`)
+//     .send({ name, details, difficulty, completed })
+//   return res.body as Task[]
+// }
+
+//-----------Decided to make API call functions inside React hooks
+//-----------as I make the React hooks
+
+export function useTasks() {
+  return useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      const res = await request.get('api/v1/tasks')
+      return res.body as Task[]
+    },
+  })
 }
 
-export async function getTaskById(id: number) {
-  const res = await request.get(`api/v1/tasks/${id}`)
-  return res.body as Task
-}
-
-export async function addTask(newTask: Task) {
-  return await request.post('api/v1/tasks').send(newTask)
-}
-
-export async function delTask(id: number) {
-  return await request.del(`api/v1/tasks/${id}`)
-}
-
-export async function updateTask(taskUpdate: Task) {
-  const { id, name, details, difficulty, completed } = taskUpdate
-  const res = await request
-    .patch(`api/v1/tasks/${id}`)
-    .send({ name, details, difficulty, completed })
-  return res.body as Task[]
+export function useTask(id: number) {
+  return useQuery({
+    queryKey: ['tasks', id],
+    queryFn: async () => {
+      const res = await request.get(`api/v1/tasks/${id}`)
+      return res.body as Task
+    },
+  })
 }
