35c60766da87ef35df43c879e565906fe386c5ec
Darragh Carraher <darraghcarraher@gmail.com>
2024-04-13 14:09:42 +1200

    home work

---
+++ server/db/migrations/20240413012323_vegetables.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('vegetables', (table) => {
+    table.increments()
+    table.string('vegetable')
+    table.boolean('rotten')
+    table.integer('price')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('vegetables')
+}
