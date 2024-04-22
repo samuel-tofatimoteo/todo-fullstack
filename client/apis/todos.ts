a1188d8094dcdf68563464f23962eef0b98d7770
Samuel Tofa-Timoteo <samtofat@gmail.com>
2024-04-23 08:56:49 +1200

    Added addTodo API call and hook

--- client/apis/todos.ts
+++ client/apis/todos.ts
@@ -1,8 +1,15 @@
 import request from 'superagent'
+import { NewTodo } from '../../models/models'
 
 const rootUrl = 'api/v1/todos'
 
+// Get all todos
 export async function getTodos() {
   const res = await request.get(`${rootUrl}`)
   return res.body
+}
+
+// Add a todo
+export async function addTodo(newTodo: NewTodo) {
+  return await request.post(`${rootUrl}`).send(newTodo)
 }
--- client/hooks/useTodos.ts
+++ client/hooks/useTodos.ts
@@ -1,10 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-import { getTodos } from "../apis/todos";
-import { query } from "express";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { addTodo, getTodos } from "../apis/todos";
+import { NewTodo } from "../../models/models";
 
 export function useTodos() {
   return useQuery({
-    queryKey: ['tasks'],
+    queryKey: ['todos'],
     queryFn: () => getTodos()
+  })
+}
+
+export function useAddTodo() {
+  const client = useQueryClient()
+
+  return useMutation({
+    mutationFn: (newTodo: NewTodo) => addTodo(newTodo) ,
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: ['todos'] })
+    },
   })
 }
